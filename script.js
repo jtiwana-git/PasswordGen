ec717440b5bc570e35b8d16a27f8e1f52942fba4
Jagdeep Tiwana <jtiwana@hotmail.com>
2022-01-07 16:15:52 +0000

    changed the if statements

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // VARIABLES / ARRAYS
-var pwdLength = [];
+var pwdLength = Number();
 var getLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",];
 var getUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",];
 var getNumber = ["0","1","2","3","4","5","6","7","8","9",];
@@ -22,15 +22,15 @@ function writePassword() {
   passwordText = pwd;
        }
   else {
-    passwordText.value = "";
+    passwordText.value = [];
 
   }
 }
 
 
 function generatePassword() {
-  var password = [] ;
-  for(var i = 0; i < pwdLength; i++) {
+  var password = [];
+  for(var i = 0; i < pwdLength; i < 128) {
     var getRandomChar = Math.floor(Math.random() * getArr.length);
     password = password + getArr[getRandomChar];
   }
@@ -40,30 +40,35 @@ function generatePassword() {
 // User to choose password length
 function ask(){
   
-  // ParasInt is to convert string into a number
-    pwdLength = Number(prompt("Enter length of your password characters - Password number range - 8 to 128"));
-      
-    // Choosing length
-  if(isNaN(pwdLength) || pwdLength < 8 || pwdLength > 128) {
-      alert("Sorry, the length must be least 8 characters and must be no more than 128 characters");
-      return false;
-  }   
+   // Choosing length
+    pwdLength = prompt("Enter the length of your password");
+  
+    if(isNaN(pwdLength) || pwdLength < 8 || pwdLength > 128) {
+      alert("Sorry, the length MUST be at least 8 character the length MUST be no MORE than 128 characters, TRY AGAIN");  
+      }  
+      else 
+        alert("Password accepted!");  
 
   // Selecting type of characters
-   if (confirm("Do you want to include LOWERCASE in your password")) {
-      getArr = getArr.concat(getLower);
+  getLower = confirm("Do you want to include LOWERCASE in your password"); 
+  getUpper = confirm("Do you want to include UPPERRCASE in your password");
+  getNumber = confirm("Do you want to include NUMBERS in your password");
+  getSpecialChar = confirm("Do you want to include SPECIAL CHARACTERS in your password"); 
+       
+  
+  if (getLower === true) {
+    getArr = getArr.concat(getLower);
     }
-   if (confirm("Do you want to include UPPERRCASE in your password")) {
+  else if (getUpper === true) {
     getArr = getArr.concat(getUpper);
     }
-
-   if (confirm("Do you want to include NUMBERS in your password")) {
+  else if (getNumber === true) {
     getArr = getArr.concat(getNumber);
     }
-
-    if (confirm("Do you want to include SPECIAL CHARACTERS in your password")) {
-      getArr = getArr.concat(getSpecialChar);
-      }
-
-      return true;
+  else if (getSpecialChar === true) { 
+    getArr = getArr.concat(getSpecialChar);
+  }
+  else {
+  alert("You need select at LEAST one type of characters, TRY AGAIN");
+  }
 }
